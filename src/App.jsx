3cc3df72fc2c0c1231747f2a0e78fec48ee0684e
Alan Santos <alanchs@live.com>
2024-03-29 18:03:27 +0800

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header';
+import { Button } from '@material-tailwind/react';
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-2xl font-bold underline">Hello world!</h1>
     </>
   );
