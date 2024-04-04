29b087d17444b4affa45f13f05ca415a48481e84
Alan Santos <alanchs@live.com>
2024-04-04 18:18:42 +0800

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -15,3 +15,9 @@ const generateToken = (res, userId) => {
 };
 
 export default generateToken;
+
+//The sameSite attribute set to 'strict' in the res.cookie()
+// method indicates that the cookie should only be sent to the
+// same site from where it originated. This means the cookie
+// will not be sent in cross-site navigation requests,
+// helping prevent Cross-Site Request Forgery (CSRF) attacks.
