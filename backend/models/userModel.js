1a1d501ee7392345749faaf1bc1b64f04815cec4
Alan Santos <alanchs@live.com>
2024-04-05 14:14:18 +0800

    added comments and using bcrypt for user password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Pre allows to do something before save in the database
+userSchema.pre('save', async function (next) {
+  // If saving a user data but not modifying the password then next
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,17 +7,21 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
+    //HTTP-only cookie is a type of cookie that can be set by a web server
+    // with a special flag that prevents it from being accessed through
+    // client-side scripts like JavaScript.
     httpOnly: true,
+
+    // The secure property in the res.cookie() method is a Boolean value that
+    // determines whether the browser should only send the cookie over HTTPS connections.
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+
+    // The sameSite attribute set to 'strict' in the res.cookie() method indicates
+    // that the cookie should only be sent to the same site from where it originated
+    // preventing CSRF attacks
+    sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
 export default generateToken;
-
-//The sameSite attribute set to 'strict' in the res.cookie()
-// method indicates that the cookie should only be sent to the
-// same site from where it originated. This means the cookie
-// will not be sent in cross-site navigation requests,
-// helping prevent Cross-Site Request Forgery (CSRF) attacks.
