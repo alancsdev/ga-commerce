fd6f996c0794b1c0930a64c6e6a8c0b13efbfdfa
Alan Santos <alanchs@live.com>
2024-04-17 19:40:18 +0800

    fixing server

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to access req.cookie
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
