faa46bbf7e4286e7064fd6f9e3a5aabbae90693c
Alan Santos <alanchs@live.com>
2024-04-17 18:19:23 +0800

    changing server to deploy

--- backend/server.js
+++ backend/server.js
@@ -31,22 +31,31 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   const __dirname = path.resolve();
+//   app.use('/uploads', express.static('/var/data/uploads'));
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-  // Any route that is not api will be redirected to index.html
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+//   // Any route that is not api will be redirected to index.html
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//   );
+// } else {
+//   const __dirname = path.resolve();
+//   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static('/var/data/uploads'));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+// Any route that is not api will be redirected to index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+);
 
 app.use(notFound);
 app.use(errorHandler);
